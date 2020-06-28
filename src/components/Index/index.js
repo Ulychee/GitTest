abfb4c8bf6caf1082c310f9dbc7ffe2212b4b3c7
Lychee <lizhi9627@gmail.com>
2020-06-28 17:47:56 +0800

    add swiper

---
+++ src/components/Index/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from '../testUseState'
+import Swiper from '../swiper'
+
+export default function Main (){
+
+    return (
+        <div>
+            <Test/>
+            <Swiper/>
+        </div>
+    )
+}
---
+++ src/components/swiper/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/swiper.scss';
+
+// import './styles.css';
+import './styles.scss';
+
+export default () => {
+    const params = {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+    }
+
+    return(
+        <div>    
+            <Swiper {...params}>
+                <div className="swiper swiper-1">#1</div>
+                <div className="swiper swiper-2">#2</div>
+                <div className="swiper swiper-3">#3</div>
+                <div className="swiper swiper-4">#4</div>
+                <div className="swiper swiper-5">#5</div>
+            </Swiper>
+        </div>
+    )
+}

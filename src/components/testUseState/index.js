da8b88f23ec3180d666f1071e4a005977ad2001e
Lychee <lizhi9627@gmail.com>
2020-07-16 18:09:56 +0800

    sss

--- src/components/testUseState/index.js
+++ src/components/testUseState/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import "./styles.scss"
 
 export default ()=>{
     const [userName,setUserName] = useState();
-    const [value,setValue] = useState("DaiMx");
+    const [value,setValue] = useState("DiDiDi");
 
     const getValue = e => {
         setUserName(e.target.value)
@@ -20,12 +21,12 @@ export default ()=>{
     }
 
     return(
-        <>
+        <div className="testBody">
             <input value={userName}  type="text" placeholder="Input your name" onChange={getValue} />
             <button type="button" onClick={handleClick}>拍一拍</button>
             <br/>
             <h2 onDoubleClick={doubleClick}>{value}</h2>
             <div id="pailepai" style={{display:"none",color:"grey"}}>ni pai le pai {value}</div>
-        </>
+        </div>
     )
 }
